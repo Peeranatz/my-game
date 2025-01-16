e6169aef26c8e246d5bd3b43e9eef3c6350e4739
Peeranat Chunhok <Peeranat280648@gmail.com>
2025-01-16 23:23:17 +0700

    เพิ่มปุ่ม Play

--- Foot_Bounce_test.py
+++ Foot_Bounce_test.py
@@ -1,36 +1,47 @@
 from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.button import Button
 from kivy.uix.image import Image
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.core.window import Window
-
-# Screen dimensions
-WIDTH, HEIGHT = 800, 600
-Window.size = (WIDTH, HEIGHT)
 
 
-class SoccerJuggleGame(Screen):
+class MainMenu(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.game_state = None
-        self.selected_player = None
-        self.score = 0
+
+        layout = FloatLayout()
 
         # Background
         self.background = Image(
-            source="bk_stadium.jpg",
-            size_hint=(None, None),
-            size=(WIDTH, HEIGHT),
-            pos=(0, 0),
+            source="main_bk_football.jpg",
+            size_hint=(1, 1),
+            allow_stretch=True,
+            keep_ratio=False,
         )
-        self.add_widget(self.background)
+        layout.add_widget(self.background)
+
+        # Start Button
+        start_button = Button(
+            text="Play",
+            size_hint=(None, None),
+            size=(200, 50),
+            pos_hint={"center_x": 0.5, "center_y": 0.5},
+        )
+        start_button.bind(on_release=self.start_game)
+        layout.add_widget(start_button)
+
+        self.add_widget(layout)
+
+    def start_game(self, instance):
+        print("Game started")
 
 
-class SoccerApp(App):
+class SoccerJuggleApp(App):
     def build(self):
         sm = ScreenManager()
-        sm.add_widget(SoccerJuggleGame(name="Foot Bounce_game"))
+        sm.add_widget(MainMenu(name="menu"))
         return sm
 
 
 if __name__ == "__main__":
-    SoccerApp().run()
+    SoccerJuggleApp().run()
